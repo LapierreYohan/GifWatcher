c0a81cca9307061e5a699b0801aa73ee9e47874a
Yohan LAPIERRE <yohan.lapierre@etu.univ-lyon1.fr>
2023-10-31 11:32:52 +0100

    SplashScreen login method

--- Gifs_Watcher/app/src/main/java/com/example/gifs_watcher/viewmodel/SplashScreenViewModel.kt
+++ Gifs_Watcher/app/src/main/java/com/example/gifs_watcher/viewmodel/SplashScreenViewModel.kt
@@ -2,9 +2,19 @@ package com.example.gifs_watcher.viewmodel
 
 import androidx.lifecycle.ViewModel
 import android.util.Patterns
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.gifs_watcher.repositories.UserRepository
 import com.example.gifswatcher.models.User
+import kotlinx.coroutines.launch
 
 class SplashScreenViewModel() : ViewModel() {
+
+    private var userRepo : UserRepository = UserRepository
+
+    private val _userLiveData: MutableLiveData<User?> = MutableLiveData()
+    val userLiveData : LiveData<User?> = _userLiveData
 
     private fun isUserNameValid(username: String): Boolean {
         return if (username.contains("@")) {
@@ -17,7 +27,14 @@ class SplashScreenViewModel() : ViewModel() {
         return password.length > 5
     }
 
-    private fun login(indentifiant : String, password : String) : Unit {
-
+    private fun login(id : String, password : String) : Unit {
+        viewModelScope.launch {
+            userRepo.verifyConnectionData(id, password)
+                .collect {
+                    it?.let { userLogged ->
+                        _userLiveData.postValue(userLogged)
+                    }
+                }
+        }
     }
 }

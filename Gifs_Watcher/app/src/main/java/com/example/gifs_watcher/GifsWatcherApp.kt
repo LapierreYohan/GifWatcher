e16da5e0bf632d0ba65dfbb0efd77648018f6d23
Yohan LAPIERRE <yohan.lapierre@mb-expansion.fr>
2023-11-16 12:23:17 +0100

    App

--- Gifs_Watcher/app/src/main/java/com/example/gifs_watcher/GifsWatcherApp.kt
+++ Gifs_Watcher/app/src/main/java/com/example/gifs_watcher/GifsWatcherApp.kt
@@ -1,6 +1,7 @@
 package com.example.gifs_watcher
 
 import android.app.Application
+import com.example.gifs_watcher.database.AppDatabase
 import timber.log.Timber
 
 class GifsWatcherApp : Application() {
@@ -8,5 +9,7 @@ class GifsWatcherApp : Application() {
         super.onCreate()
 
         Timber.plant(Timber.DebugTree())
+
+        AppDatabase.initDatabase(context = applicationContext)
     }
 }

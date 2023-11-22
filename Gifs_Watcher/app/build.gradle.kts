469aefe848d6471b2a1f42f50dba7fffbb63b223
Axel GAILLIARD <>
2023-11-22 14:17:52 +0100

    Glide Gif implement
    implementation de Glide dans les gradles / test des gifs via un drawable

--- Gifs_Watcher/app/build.gradle.kts
+++ Gifs_Watcher/app/build.gradle.kts
@@ -76,6 +76,10 @@ dependencies {
     implementation("androidx.annotation:annotation:1.6.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
 
+    // Glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- Gifs_Watcher/app/src/main/java/com/example/gifs_watcher/views/main/fragments/HomeFragment.kt
+++ Gifs_Watcher/app/src/main/java/com/example/gifs_watcher/views/main/fragments/HomeFragment.kt
@@ -5,11 +5,15 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.example.gifs_watcher.R
 import com.example.gifs_watcher.databinding.FragmentHomeBinding
 import com.example.gifs_watcher.viewmodel.MainViewModel
+import com.squareup.picasso.Picasso
 
 class HomeFragment : Fragment() {
 
@@ -25,10 +29,26 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         Log.println(Log.INFO,"debug","on create home")
-        val mainViewModel_ =
-            ViewModelProvider(this).get(MainViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+
+        val mainViewModel_ = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        try {
+            _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        } catch (e: Exception) {
+            Log.println(Log.ERROR,"debug","on create home error : " + e.message)
+        }
+
+        try {
+            val Gif: ImageView = binding.Gif
+            val backgroundGif: ImageView = binding.backgroundGif
+            //Picasso.get().load(R.drawable.bobawooyo_dog_confused).into(Gif)
+            Glide.with(this).load(R.drawable.bobawooyo_dog_confused).into(Gif)
+            Glide.with(this).load(R.drawable.bobawooyo_dog_confused).centerCrop().into(backgroundGif)
+        }catch (e: Exception) {
+            Log.println(Log.ERROR,"debug","Gif create error : " + e.message)
+        }
 
         val root: View = binding.root
 
